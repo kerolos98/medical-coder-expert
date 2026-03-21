defdd53d588c2eda1e662e565dfc4a297562ce8a
kerolos98 <kemad57gmail.com>
2026-03-21 03:47:21 +0200

    fix config paths

--- app/model_input_files_config.py
+++ app/model_input_files_config.py
@@ -1,7 +1,12 @@
 import os
+
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
-FOLDER_NAME = "/data/embeddings"
+
+FOLDER_NAME = "data/embeddings" 
 FOLDER_PATH = os.path.join(BASE_PATH, FOLDER_NAME)
+
+# Ensure directory exists
+os.makedirs(FOLDER_PATH, exist_ok=True)
 
 EMBEDDINGS_MODEL = f"{FOLDER_PATH}/sapbert"
 SNOMED_EMBEDDINGS = f"{FOLDER_PATH}/sapbert_snomed_embeddings.h5"
@@ -10,5 +15,5 @@ ICD10_EMBEDDINGS = f"{FOLDER_PATH}/SapBERT_diag_embeddings.h5"
 ICD10_PCA = f"{FOLDER_PATH}/pca_768_to_256_diag.json"
 RX_EMBEDDINGS = f"{FOLDER_PATH}/sapbert_rx_embeddings_95var.h5"
 RX_PCA = f"{FOLDER_PATH}/pca_768_to_95var_rx.json"
-WEIGHTS_DIR = "/data/embeddings"
-API_KEYS_DB_PATH = "/data/embeddings/api_keys.db"
+
+API_KEYS_DB_PATH = os.path.join(FOLDER_PATH, "api_keys.db")  
