1d509f31e4c5776f012070caaccfadaf1ff3827f
kerolos98 <kemad57gmail.com>
2026-03-21 04:36:07 +0200

    fix config paths

--- app/model_input_files_config.py
+++ app/model_input_files_config.py
@@ -1,7 +1,4 @@
-import os
-
 FOLDER_PATH = "/data/embeddings" 
-
 EMBEDDINGS_MODEL = f"{FOLDER_PATH}/sapbert"
 SNOMED_EMBEDDINGS = f"{FOLDER_PATH}/sapbert_snomed_embeddings.h5"
 SNOMED_PCA = f"{FOLDER_PATH}/pca_768_to_256_SNOMED.json"
@@ -9,5 +6,4 @@ ICD10_EMBEDDINGS = f"{FOLDER_PATH}/SapBERT_diag_embeddings.h5"
 ICD10_PCA = f"{FOLDER_PATH}/pca_768_to_256_diag.json"
 RX_EMBEDDINGS = f"{FOLDER_PATH}/sapbert_rx_embeddings_95var.h5"
 RX_PCA = f"{FOLDER_PATH}/pca_768_to_95var_rx.json"
-
-API_KEYS_DB_PATH = os.path.join(FOLDER_PATH, "api_keys.db")
+API_KEYS_DB_PATH = f"{FOLDER_PATH}/api_keys.db"
