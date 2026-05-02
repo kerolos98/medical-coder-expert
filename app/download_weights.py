e3f728ee739b1549afecfb32bc9ad936a1c6274f
kerolos emad <33869460+kerolos98@users.noreply.github.com>
2026-05-02 21:32:02 +0300

    Fix download_weights function to use correct URL

--- app/download_weights.py
+++ app/download_weights.py
@@ -7,13 +7,12 @@ def download_weights():
     if not os.path.exists(WEIGHTS_PATH):
         os.makedirs(WEIGHTS_PATH, exist_ok=True)
         subprocess.run([
-            "gdown",
-            "--fuzzy",
-            "--folder",  # use only if downloading a folder
-            DRIVE_URL,
-            "-O",
-            WEIGHTS_PATH
-        ], check=True)
+        "gdown",
+        "--folder",
+        "URL",
+        "-O",
+        WEIGHTS_PATH
+    ])
         print("Download complete.")
     else:
         print("Weights already exist.")
